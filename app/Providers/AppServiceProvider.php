6ed23b2a18fee5720ba531d4a697e16bf8fb7579
drh <jmnabangi@gmail.com>
2022-07-02 13:27:35 +0300

    adds lazy loading prevention in dev

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,8 +27,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         JsonResource::withoutWrapping();
-    }
-    public function boot() {
+        
         Model::preventLazyLoading(!app()->isProduction());
     }
 }
