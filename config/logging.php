7577462db3cda205bb153d806d085611ce82285c
Nabcellent <nabcellent.dev@gmail.com>
2023-02-25 21:26:39 +0300

    Reverts Develop
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

--- config/logging.php
+++ config/logging.php
@@ -104,8 +104,7 @@ return [
             'driver'    => 'monolog',
             'level'     => env('LOG_LEVEL', 'debug'),
             'handler'   => StreamHandler::class,
-            //            'formatter' => env('LOG_STDERR_FORMATTER'),
-            'formatter' => GoogleCloudLoggingFormatter::class,
+            'formatter' => env('LOG_STDERR_FORMATTER'),
             'with'      => [
                 'stream' => 'php://stderr',
             ],
