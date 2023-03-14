36d71db7d74716cc960942d4c457e15eb9a313f4
Nabcellent <nabcellent.dev@gmail.com>
2023-03-14 05:02:31 +0300

    Enables Voucher Top-up using till payment.
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

--- app/Listeners/C2bPaymentConfirmation.php
+++ app/Listeners/C2bPaymentConfirmation.php
@@ -39,23 +39,3 @@ class C2bPaymentConfirmation implements ShouldQueue
         }
     }
 }
-
-//  C2B Payment Callback
-/*{
-    invoice_number: null
-    trans_time: "20230311102523"
-    bill_ref_number: null
-    msisdn: "254110039317"
-    middle_name: "otunga"
-    transaction_type: "Customer Merchant Payment"
-    org_account_balance: "6159.95"
-    first_name: "Michael"
-    created_at: "2023-03-11T07:25:25.000000Z"
-    third_party_trans_id: null
-    updated_at: "2023-03-11T07:25:25.000000Z"
-    business_short_code: "7400550"
-    trans_amount: "20.00"
-    trans_id: "RCB3IXGWAJ"
-    last_name: "nabangi"
-    id: 83
-}*/
