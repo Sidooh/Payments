9e79312c61aa79675e2059a7cbea5df052934fad
drh <jmnabangi@gmail.com>
2023-04-07 18:13:00 +0300

    adds caching to merchant code fetch
    Signed-off-by: drh <jmnabangi@gmail.com>

--- app/Http/Controllers/API/V1/TendePayMerchantController.php
+++ app/Http/Controllers/API/V1/TendePayMerchantController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API\V1;
 
 use App\Http\Controllers\Controller;
 use App\Models\TendePayMerchant;
+use Cache;
 use Illuminate\Http\JsonResponse;
 
 class TendePayMerchantController extends Controller
@@ -11,7 +12,13 @@ class TendePayMerchantController extends Controller
     //
     public function searchMerchant(int $code): JsonResponse
     {
-        $merchant = TendePayMerchant::select('code', 'name')->whereCode($code)->firstOrFail();
+        $merchant = Cache::get("merchant_$code");
+
+        if (!$merchant) {
+            $merchant = TendePayMerchant::select('code', 'name')->whereCode($code)->firstOrFail();
+
+            Cache::forever("merchant_$code", $merchant);
+        }
 
         return $this->successResponse($merchant);
     }
