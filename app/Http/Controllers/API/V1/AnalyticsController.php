d77c2a0e5bea76b3d374a78285ba8be03d153456
DrH <jmnabangi@gmail.com>
2024-03-30 11:24:56 +0300

    Update to Laravel 11
    Signed-off-by: DrH <jmnabangi@gmail.com>

--- app/Http/Controllers/API/V1/AnalyticsController.php
+++ app/Http/Controllers/API/V1/AnalyticsController.php
@@ -6,6 +6,7 @@ use App\Enums\PaymentSubtype;
 use App\Enums\Status;
 use App\Http\Controllers\Controller;
 use App\Models\Payment;
+use DrH\Mpesa\Entities\MpesaB2bCallback;
 use DrH\Mpesa\Entities\MpesaBulkPaymentResponse;
 use DrH\Mpesa\Entities\MpesaStkRequest;
 use DrH\TendePay\Models\TendePayRequest;
@@ -17,12 +18,12 @@ class AnalyticsController extends Controller
 {
     public function getPaymentsSLO(): JsonResponse
     {
-        $slo = Cache::remember('payments_slo', (3600 * 24 * 7), function() {
+        $slo = Cache::remember('payments_slo', (3600 * 24 * 7), function () {
             return Payment::selectRaw('YEAR(created_at) as year, status, count(*) as count')
-                          ->groupByRaw('year, status')
-                          ->get()
-                          ->filter(fn ($x) => in_array($x->status,
-                              [Status::COMPLETED, Status::FAILED]));
+                ->groupByRaw('year, status')
+                ->get()
+                ->filter(fn($x) => in_array($x->status,
+                    [Status::COMPLETED, Status::FAILED]));
         });
 
         return $this->successResponse($slo);
@@ -30,23 +31,27 @@ class AnalyticsController extends Controller
 
     public function getVendorsSLO(Request $request): JsonResponse
     {
-        $SLOs = Cache::remember('vendors_slo', (3600 * 24 * 7), fn () => [
+        $SLOs = Cache::remember('vendors_slo', (3600 * 24 * 7), fn() => [
             'tende'     => TendePayRequest::selectRaw('ROUND(COUNT(status)/COUNT(*) * 100) slo')
-                                          ->fromRaw("(SELECT CASE WHEN status = '1' THEN 1 END status FROM tende_pay_requests WHERE created_at > ?) tende_pay_requests",
-                                              now()->subYear())
-                                          ->value('slo'),
+                ->fromRaw("(SELECT CASE WHEN status = '1' THEN 1 END status FROM tende_pay_requests WHERE created_at > ?) tende_pay_requests",
+                    now()->subYear())
+                ->value('slo'),
             'mpesa_stk' => MpesaStkRequest::selectRaw('ROUND(COUNT(status)/COUNT(*) * 100) slo')
-                                          ->fromRaw("(SELECT CASE WHEN status = 'Paid' THEN 1 END status FROM mpesa_stk_requests WHERE created_at > ?) mpesa_stk_requests",
-                                              now()->subYear())
-                                          ->value('slo'),
+                ->fromRaw("(SELECT CASE WHEN status = 'Paid' THEN 1 END status FROM mpesa_stk_requests WHERE created_at > ?) mpesa_stk_requests",
+                    now()->subYear())
+                ->value('slo'),
             'mpesa_b2c' => MpesaBulkPaymentResponse::selectRaw('ROUND(COUNT(result_code)/COUNT(*) * 100) slo')
-                                                   ->fromRaw("(SELECT CASE WHEN result_code = '0' THEN 1 END result_code FROM mpesa_bulk_payment_responses WHERE created_at > ?) mpesa_bulk_payment_responses",
-                                                       now()->subYear())
-                                                   ->value('slo'),
+                ->fromRaw("(SELECT CASE WHEN result_code = '0' THEN 1 END result_code FROM mpesa_bulk_payment_responses WHERE created_at > ?) mpesa_bulk_payment_responses",
+                    now()->subYear())
+                ->value('slo'),
+            'mpesa_b2b' => MpesaB2bCallback::selectRaw('ROUND(COUNT(result_code)/COUNT(*) * 100) slo')
+                ->fromRaw("(SELECT CASE WHEN result_code = '0' THEN 1 END result_code FROM mpesa_b2b_callbacks WHERE created_at > ?) mpesa_bulk_payment_responses",
+                    now()->subYear())
+                ->value('slo'),
             'voucher'   => Payment::selectRaw('ROUND(COUNT(status)/COUNT(*) * 100) slo')
-                                  ->fromRaw("(SELECT CASE WHEN status = 'COMPLETED' THEN 1 END status FROM payments WHERE subtype = ? OR destination_subtype = ?) payments",
-                                      [PaymentSubtype::VOUCHER, PaymentSubtype::VOUCHER])
-                                  ->value('slo'),
+                ->fromRaw("(SELECT CASE WHEN status = 'COMPLETED' THEN 1 END status FROM payments WHERE subtype = ? OR destination_subtype = ?) payments",
+                    [PaymentSubtype::VOUCHER, PaymentSubtype::VOUCHER])
+                ->value('slo'),
         ]);
 
         return $this->successResponse($SLOs);
@@ -54,11 +59,11 @@ class AnalyticsController extends Controller
 
     public function payments(): JsonResponse
     {
-        $data = Cache::remember('transactions', (3600 * 24), function() {
+        $data = Cache::remember('transactions', (3600 * 24), function () {
             return Payment::selectRaw("status, DATE_FORMAT(created_at, '%Y%m%d%H') as date, COUNT(*) as count, SUM(amount) as amount")
-                          ->groupBy('date', 'status')
-                          ->orderByDesc('date')
-                          ->get();
+                ->groupBy('date', 'status')
+                ->orderByDesc('date')
+                ->get();
         });
 
         return $this->successResponse($data);
---
+++ database/migrations/2024_03_27_083246_create_jenga_bill_ipns_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up()
+    {
+        Schema::create('jenga_bill_ipns', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('bill_number')->index();
+            $table->decimal('bill_amount');
+            $table->string('customer_ref_number');
+            $table->string('bankreference');
+            $table->string('tran_particular');
+            $table->string('payment_mode');
+            $table->string('transaction_date');
+            $table->string('phonenumber');
+            $table->string('debitaccount');
+            $table->string('debitcustname');
+
+            $table->timestamps();
+        });
+    }
+};
