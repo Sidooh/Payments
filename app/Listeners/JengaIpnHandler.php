106f568d05b106600bed9875d72bb91dead36d99
DrH <jmnabangi@gmail.com>
2024-04-22 15:53:13 +0300

    Disable Jenga IPN handler
    Signed-off-by: DrH <jmnabangi@gmail.com>

--- app/Listeners/JengaIpnHandler.php
+++ app/Listeners/JengaIpnHandler.php
@@ -2,22 +2,19 @@
 
 namespace App\Listeners;
 
-use App\Repositories\EventRepositories\JengaEventRepository;
 use DrH\Jenga\Events\JengaIpnEvent;
-use Exception;
 use Illuminate\Support\Facades\Log;
-use Throwable;
 
 class JengaIpnHandler
 {
     public function handle(JengaIpnEvent $event): void
     {
-        Log::info('...[EVENT]: Jenga IPN Received...');
+        Log::info('...[EVENT]: Jenga IPN Received... -- disabled');
 
-        try {
-            JengaEventRepository::ipnReceived($event->ipn);
-        } catch (Exception|Throwable $e) {
-            Log::critical($e);
-        }
+//        try {
+//            JengaEventRepository::ipnReceived($event->ipn);
+//        } catch (Exception|Throwable $e) {
+//            Log::critical($e);
+//        }
     }
 }
